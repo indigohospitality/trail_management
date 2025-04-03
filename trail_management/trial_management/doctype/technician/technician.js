88bcc337b64e6b563b7c800edbcd6fd3f62533ca
donboscokunnath <aroma247erp@gmail.com>
2025-04-03 07:56:09 +0400

    changes added
    changes added

---
+++ trail_management/trial_management/doctype/technician/technician.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Don Bosco Peter and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("technician", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ trail_management/trial_management/doctype/trail_machines/test_trail_machines.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Don Bosco Peter and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class Testtrailmachines(FrappeTestCase):
+	pass
